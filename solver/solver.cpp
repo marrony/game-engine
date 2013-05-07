7a7f0d1dae1d382471d072e81acf28af2be27f9f
Marrony Neris <neris.marrony@gmail.com>
2013-05-06 23:07:25 -0300

    refactoring on solver

--- solver/solver.cpp
+++ solver/solver.cpp
@@ -37,7 +37,7 @@
 #define USE_VERBOSE
 
 #define NUM_SOLVER_ITERATIONS 20
-#define NUM_JACOBIANS         2
+#define NUM_JACOBIANS         3
 #define CONVERGENCE_ESTIMATE  1.0f
 
 struct SimObject {
@@ -67,17 +67,23 @@ int main(void) {
 	Jacobian jacobians[NUM_JACOBIANS];
 
 	jacobians[0].m_linear = vector::make(0.0f, 1.0f, 0.0f);
-	jacobians[1].m_linear = vector::make(0.0f, 0.5f, 0.5f);
+	jacobians[1].m_linear = vector::make(0.0f, 0.0f, 1.0f);
+	jacobians[2].m_linear = vector::make(1.0f, 0.0f, 0.0f);
 	jacobians[0].m_constraint_force = 1.0f;
-	jacobians[1].m_constraint_force = 1.2f;
+	jacobians[1].m_constraint_force = 10.0f;
+	jacobians[2].m_constraint_force = 1.8f;
 	jacobians[0].m_limit_lower = 0.0f;
 	jacobians[1].m_limit_lower = 0.0f;
+	jacobians[2].m_limit_lower = 0.0f;
 	jacobians[0].m_limit_upper = FLT_MAX;
 	jacobians[1].m_limit_upper = FLT_MAX;
+	jacobians[2].m_limit_upper = FLT_MAX;
 	jacobians[0].m_mixing_coefficient = 0.0001f;
 	jacobians[1].m_mixing_coefficient = 0.0001f;
+	jacobians[2].m_mixing_coefficient = 0.0001f;
 	jacobians[0].m_object_index = 0;
 	jacobians[1].m_object_index = 0;
+	jacobians[2].m_object_index = 0;
 
 	for (int x = 0; x < NUM_JACOBIANS; x++) {
 		jacobians[x].m_linear = jacobians[x].m_linear.normalize();
@@ -150,15 +156,13 @@ void Solve3DVerbose(Jacobian *j, int num_jacobians, SimObject *o, int num_object
 	float *b = (float *) malloc(sizeof(float) * num_jacobians); // right hand sides
 	float *D = (float *) malloc(sizeof(float) * num_jacobians); // diagonal pre-scale
 	float *D_ = (float *) malloc(sizeof(float) * num_jacobians); // diagonal post-scale
-	float *lam = (float *) malloc(sizeof(float) * num_jacobians); // lambda
-	Vector3 *jlin_ = (Vector3 *) malloc(sizeof(Vector3) * num_jacobians); // scaled jacobian
-	Vector3 *a = (Vector3 *) malloc(sizeof(Vector3) * num_objects); // corrective acceleration
+	float *L = (float *) malloc(sizeof(float) * num_jacobians); // lambda
+	Vector3 *Js = (Vector3 *) malloc(sizeof(Vector3) * num_jacobians); // scaled jacobian
+	Vector3* JM_1 = (Vector3 *) malloc(sizeof(Vector3) * num_jacobians); // JM^-1
+	Vector3 *velocity = (Vector3 *) malloc(sizeof(Vector3) * num_objects); // corrective acceleration
 
-	memset(lam, 0, sizeof(float) * num_jacobians);
-	memset(a, 0, sizeof(Vector3) * num_objects);
-
-	float lam_; // temp lambda
-	float d;    // delta
+	memset(L, 0, sizeof(float) * num_jacobians);
+	memset(velocity, 0, sizeof(Vector3) * num_objects);
 
 	printf("Input:\n");
 
@@ -182,7 +186,7 @@ void Solve3DVerbose(Jacobian *j, int num_jacobians, SimObject *o, int num_object
 	for (int x = 0; x < num_jacobians; x++) {
 		printf("\tJacobian (%d)\n", x);
 
-		Vector3 JM_1 = j[x].m_linear * o[j[x].m_object_index].m_invMass;
+		JM_1[x] = j[x].m_linear * o[j[x].m_object_index].m_invMass;
 
 		float u = vector::dot(o[j[x].m_object_index].m_velocity, j[x].m_linear);
 		float v = 0;
@@ -190,13 +194,13 @@ void Solve3DVerbose(Jacobian *j, int num_jacobians, SimObject *o, int num_object
 		b[x] = u + v;
 		b[x] = j[x].m_constraint_force - b[x];
 
-		D[x] = vector::dot(JM_1, j[x].m_linear);
+		D[x] = vector::dot(JM_1[x], j[x].m_linear);
 		D[x] = D[x] + j[x].m_mixing_coefficient;
 		D[x] = 1.0f / D[x];
 
 		b[x] = b[x] * D[x];
 
-		jlin_[x] = j[x].m_linear * D[x];
+		Js[x] = j[x].m_linear * D[x];
 
 		D_[x] = D[x] * j[x].m_mixing_coefficient;
 
@@ -214,37 +218,37 @@ void Solve3DVerbose(Jacobian *j, int num_jacobians, SimObject *o, int num_object
 		for (int j_index = 0; j_index < num_jacobians; j_index++) {
 			printf("\t\tjacobian (%d)\n", j_index);
 
-			d = (b[j_index] - lam[j_index] * D_[j_index]) - vector::dot(a[j[j_index].m_object_index], jlin_[j_index]);
+			float delta = (b[j_index] - L[j_index] * D_[j_index]) - vector::dot(velocity[j[j_index].m_object_index], Js[j_index]);
 
-			printf("\t\t\td = %f\n", d);
+			printf("\t\t\td = %f\n", delta);
 
-			lam_ = lam[j_index] + d;
+			float L_test = L[j_index] + delta;
 
-			printf("\t\t\tlam_ = %f\n", lam_);
+			printf("\t\t\tlam_ = %f\n", L_test);
 
-			if (lam_ < j[j_index].m_limit_lower) {
+			if (L_test < j[j_index].m_limit_lower) {
 				printf("\t\t\tlim: hit lower limit of %f\n", j[j_index].m_limit_lower);
-				d = j[j_index].m_limit_lower - lam[j_index];
-				printf("\t\t\t\td = %f\n", d);
-				lam[j_index] = j[j_index].m_limit_lower;
-			} else if (lam_ > j[j_index].m_limit_upper) {
+				delta = j[j_index].m_limit_lower - L[j_index];
+				printf("\t\t\t\td = %f\n", delta);
+				L[j_index] = j[j_index].m_limit_lower;
+			} else if (L_test > j[j_index].m_limit_upper) {
 				printf("\t\t\tlim: hit upper limit of %f\n", j[j_index].m_limit_upper);
-				d = j[j_index].m_limit_upper - lam[j_index];
-				printf("\t\t\t\td = %f\n", d);
-				lam[j_index] = j[j_index].m_limit_upper;
+				delta = j[j_index].m_limit_upper - L[j_index];
+				printf("\t\t\t\td = %f\n", delta);
+				L[j_index] = j[j_index].m_limit_upper;
 			} else {
 				printf("\t\t\tlim: within bounds\n");
-				lam[j_index] = lam_;
+				L[j_index] = L_test;
 			}
 
-			a[j[j_index].m_object_index] += j[j_index].m_linear * d;
+			velocity[j[j_index].m_object_index] += (JM_1[j_index] * delta);
 
-			printf("\t\t\taccel (object %d) = (%f %f %f)\n", j[j_index].m_object_index, a[j[j_index].m_object_index].x, a[j[j_index].m_object_index].y, a[j[j_index].m_object_index].z);
+			printf("\t\t\taccel (object %d) = (%f %f %f)\n", j[j_index].m_object_index, velocity[j[j_index].m_object_index].x, velocity[j[j_index].m_object_index].y, velocity[j[j_index].m_object_index].z);
 		}
 	}
 
 	for (int obj_index = 0; obj_index < num_objects; obj_index++) {
-		o[obj_index].m_velocity += a[obj_index];
+		o[obj_index].m_velocity += velocity[obj_index];
 	}
 
 	printf("Output:\n");
@@ -253,9 +257,10 @@ void Solve3DVerbose(Jacobian *j, int num_jacobians, SimObject *o, int num_object
 		printf("\t\tVelocity = (%f %f %f)\n", o[obj_index].m_velocity.x, o[obj_index].m_velocity.y, o[obj_index].m_velocity.z);
 	}
 
-	free(a);
-	free(jlin_);
-	free(lam);
+	free(velocity);
+	free(Js);
+	free(JM_1);
+	free(L);
 	free(D_);
 	free(D);
 	free(b);
